155467e4fa27c176342184d76423eee3493c5d76
sanginjeong <sanginjeong07@gmail.com>
2025-09-27 12:06:32 +0900

    hotfix

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -16,9 +16,7 @@ const HomePage = () => {
       <h2 className="text-blue-400 font-bold text-[22px]">
         나의 동물 성격 찾기
       </h2>
-      <h3 className="font-bold">
-        {questions.length}개의 질문을 통해 당신과 닮은 동물을 찾아보세요!
-      </h3>
+      <h3>{questions.length}개의 질문을 통해 당신과 닮은 동물을 찾아보세요!</h3>
       <ul className="list-disc text-gray-400">
         <li>정확한 결과를 위해 솔직하게 답변해주세요</li>
         <li>약 2분 정도 소요됩니다</li>
